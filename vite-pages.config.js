2b3a74671ed0078570fec5ae9b63b6e8507c0876
Michel Albert <michel@albert.lu>
2022-04-15 07:02:04 +0200

    Add mozilla "background page"

--- vite-pages.config.js
+++ vite-pages.config.js
@@ -5,6 +5,9 @@ export default {
     outDir: "build/pages",
     rollupOptions: {
       input: {
+        background: fileURLToPath(
+          new URL("./src/core/background/index.html", import.meta.url)
+        ),
         sidebar: fileURLToPath(
           new URL("./src/views/sidebar/index.html", import.meta.url)
         ),
