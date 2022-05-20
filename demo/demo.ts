02d0ad8bf4f4f1246a7f444c0c9536848f816c47
Michel Albert <michel@albert.lu>
2022-05-20 12:29:49 +0000

    Better name for the demo page

--- demo/demo.ts
+++ demo/demo.ts
@@ -1,5 +1,8 @@
 import "../src/components/views/sh-settings";
-import { Settings } from "./src/components/views/sh-settings";
+import { Settings } from "../src/components/views/sh-settings";
+import { Links } from "../src/components/core/links";
+import { Link } from "../src/components/model/link";
+
 let settingsElementV1 = document.getElementById("SettingsV1") as Settings;
 let settingsElementV2 = document.getElementById("SettingsV2") as Settings;
 settingsElementV2.settings = JSON.stringify({
@@ -18,3 +21,15 @@ settingsElementV1.settings = JSON.stringify({
   remoteUrl: "https://raw.githubusercontent.com/exhuma/dotfiles/master/bookmarks.json",
   version: 1,
 });
+
+let tagListElement = document.getElementById("TagList");
+tagListElement.links = new Links([
+  new Link(
+    "https://example.com",
+    ["tag1", "tag2"],
+    "Example Link",
+    "image-url",
+    "description"
+    )
+  ]
+)
