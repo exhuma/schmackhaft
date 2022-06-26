2d4c51a23373640a0bfce72cae9ff0579b15677b
Michel Albert <michel@albert.lu>
2022-06-26 15:34:33 +0200

    Add a data-class for application settings

--- src/core/settings.ts
+++ src/core/settings.ts
@@ -1,6 +1,36 @@
 import * as browser from "webextension-polyfill";
 import { IStorageBackend } from "../types";
 
+
+export class Settings {
+  remoteUrls: string[];
+  enableBrowserBookmarks: boolean;
+  version: number;
+
+  constructor(remoteUrls: string[], enableBrowserBookmarks: boolean, version: number) {
+    this.remoteUrls = remoteUrls;
+    this.enableBrowserBookmarks = enableBrowserBookmarks;
+    this.version = version;
+  }
+
+  toJson(): string {
+    return JSON.stringify(this);
+  }
+
+  static fromJson(data: string): Settings {
+    let dataObject = JSON.parse(data);
+    return new Settings(
+      dataObject.remoteUrls,
+      dataObject.enableBrowserBookmarks,
+      dataObject.version
+    );
+  }
+}
+
+
+/**
+ * A bridge to a "storage" for application settings.
+ */
 export class SettingsBridge {
   backend: any;
   static async default() {
