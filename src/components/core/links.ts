4d33586340c81b976b3f16f69334c839073eceb8
Michel Albert <michel@albert.lu>
2022-07-05 17:03:09 +0200

    Add more docstrings

--- src/components/core/links.ts
+++ src/components/core/links.ts
@@ -1,3 +1,8 @@
+/**
+ * This file contains the definition for a collection of links. The collection
+ * provides implementations for searching links on various conditions.
+ */
+
 import { Link, JsonSchema as LinkSchema } from "../../model/link";
 import { TagState } from "../../types";
 
@@ -11,28 +16,56 @@ function intersection(setA: Array<string>, setB: Array<string>) {
   return _intersection;
 }
 
+/**
+ * A colelction of links.
+ *
+ * This is the main implementation for working and search for links.
+ */
 export class Links {
   links: Array<Link>;
   searchString: string = "";
   states: { [key: string]: TagState };
 
+  /**
+   * @param links An initial collection of links. Can be an empty array.
+   */
   constructor(links: Array<Link> = []) {
     this.links = links;
     this.states = {};
   }
 
+  /**
+   * Instantiate a collection of links from a JSON string.
+   *
+   * The JSON string must have a top-level list of items. Each item is loaded
+   * via Link.fromObject and must conform to the schema required by it.
+   *
+   * @param data A JSON string
+   * @returns A new instance of a "Links" object
+   */
   static fromJson(data: string): Links {
     let parsed = JSON.parse(data);
     let links = parsed.map((item: LinkSchema) => Link.fromObject);
     return new Links(links);
   }
 
+  /**
+   * Convert this collection of links to a JSON string
+   * @returns A JSON string
+   */
   toJson(): string {
     return JSON.stringify(
       this.links.map((item): LinkSchema => item.toObject())
     );
   }
 
+  /**
+   * Return a new list of links acording to the specified filter(s) in this class.
+   * The returned list will also be sorted according to the link's "compString"
+   * argument
+   *
+   * @todo Is it not possible to make objects comparable in JS? It should be?
+   */
   get filtered() {
     const output = this.links.filter((link) => {
       return this.isMatchingOnTags(link) && this.isMatchingOnSearchString(link);
@@ -43,6 +76,12 @@ export class Links {
     return output;
   }
 
+  /**
+   * Determines whether a user searching for the string stored in this "Links"
+   * class should see the given link.
+   * @param link The link we compare
+   * @returns  Whether the link matches the string or not.
+   */
   isMatchingOnSearchString(link: Link) {
     const regex = new RegExp(this.searchString, "i");
     return (
@@ -52,6 +91,10 @@ export class Links {
     );
   }
 
+  /**
+   * Return all the searched tags that are currently "included" when filtering
+   * using this "Links" class.
+   */
   get searchedTags() {
     let output: string[] = [];
     Object.entries(this.states).forEach(([key, value]) => {
@@ -62,6 +105,10 @@ export class Links {
     return output;
   }
 
+  /**
+   * Return all the searched tags that are currently "excluded" when filtering
+   * using this "Links" class.
+   */
   get excludedTags() {
     let output: string[] = [];
     Object.entries(this.states).forEach(([key, value]) => {
@@ -72,6 +119,12 @@ export class Links {
     return output;
   }
 
+  /**
+   * Determines the current "mode" of a given tag in this collection.
+   *
+   * @param tagName The tag we want to inspect
+   * @returns A TagState instance
+   */
   getState(tagName: string): TagState {
     let output = TagState.NEUTRAL;
     if (this.links) {
@@ -80,6 +133,11 @@ export class Links {
     return output;
   }
 
+  /**
+   * @param link The link we inspect
+   * @returns Whether the link should be included in a result when searching on
+   * tags or not.
+   */
   isMatchingOnTags(link: Link) {
     if (!this.states || Object.keys(this.states).length === 0) {
       return true;
@@ -96,11 +154,20 @@ export class Links {
     return output;
   }
 
+  /**
+   * Remove all search filters and/or set them to their default values.
+   */
   reset() {
     this.clearSearch();
     this.states = {};
   }
 
+  /**
+   * Modify how a given tag affects the current search. This is multi-state and
+   * this method will "move" the state in one direction. See "reverseState"
+   * for the inverse operation.
+   * @param tagName The tag we want to modify
+   */
   advanceState(tagName: string) {
     let currentState = this.states[tagName] ?? TagState.NEUTRAL;
     let newState = TagState.NEUTRAL;
@@ -121,6 +188,12 @@ export class Links {
     this.states[tagName] = newState;
   }
 
+  /**
+   * Modify how a given tag affects the current search. This is multi-state and
+   * this method will "move" the state in the opposite direction of
+   * "advanceState".
+   * @param tagName The tag we want to modify
+   */
   reverseState(tagName: string) {
     let currentState = this.states[tagName] ?? TagState.NEUTRAL;
     let newState = TagState.NEUTRAL;
@@ -141,10 +214,17 @@ export class Links {
     this.states[tagName] = newState;
   }
 
+  /**
+   * Set the search-string of this collection
+   * @param substring The search string
+   */
   search(substring: string) {
     this.searchString = substring;
   }
 
+  /**
+   * Remove the search string from this collection
+   */
   clearSearch() {
     this.searchString = "";
   }
