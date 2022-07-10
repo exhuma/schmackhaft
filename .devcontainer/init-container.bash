649f05717266ef9c03654f5a154f4b3a4f250120
Michel Albert <michel@albert.lu>
2022-07-10 16:47:01 +0000

    Add dev-requirement to the dev-container

--- .devcontainer/init-container.bash
+++ .devcontainer/init-container.bash
@@ -1,3 +1,6 @@
 #!/bin/bash
 set -xe
+pip install --user pipx
+pipx install pre-commit
 npm clean-install
+pre-commit install
--- .devcontainer/init-personal.bash
+++ .devcontainer/init-personal.bash
@@ -1,6 +1,3 @@
 #!/bin/bash
 set -xe
-sudo apt update
-sudo apt -y install \
-    vim-nox
-git clone https://github.com/exhuma/dotfiles /home/node/dotfiles
+curl https://raw.githubusercontent.com/exhuma/dotfiles/master/bootstrap_devcontainer.bash | bash -
