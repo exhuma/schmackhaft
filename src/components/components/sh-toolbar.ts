d1eed8a1a1d6772dd2e6e6ca99173e057d19658c
Michel Albert <michel@albert.lu>
2022-07-15 13:10:24 +0000

    Add tooltips to existing buttons

--- src/components/components/sh-toolbar.ts
+++ src/components/components/sh-toolbar.ts
@@ -106,6 +106,7 @@ export class Toolbar extends LitElement {
 
           <button
             type="button"
+            title="Display all bookmarks"
             @click="${() => this.sendEvent(ToolbarAction.BOOKMARKS)}"
             class="py-1 px-4 text-sm font-medium text-gray-900 bg-white border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white"
           >
@@ -113,6 +114,7 @@ export class Toolbar extends LitElement {
           </button>
           <button
             type="button"
+            title="Configure Schmackhaft"
             @click="${() => this.sendEvent(ToolbarAction.SETTINGS)}"
             class="py-1 px-4 text-sm font-medium text-gray-900 bg-white border-t border-b border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white"
           >
@@ -120,6 +122,7 @@ export class Toolbar extends LitElement {
           </button>
           <button
             type="button"
+            title="Display Help"
             @click="${() => this.sendEvent(ToolbarAction.HELP)}"
             class="py-1 px-4 text-sm font-medium text-gray-900 bg-white rounded-r-sm border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white"
           >
